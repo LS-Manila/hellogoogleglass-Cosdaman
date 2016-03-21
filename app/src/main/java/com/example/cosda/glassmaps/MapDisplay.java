ca87c0be4a387a486bb58a7d3ecd7e89d9132993
Cosdaman <Cosdaguy@gmail.com>
2016-03-21 15:48:27 +0800

    more experiments
    check notes

--- app/src/main/java/com/example/cosda/glassmaps/MapDisplay.java
+++ app/src/main/java/com/example/cosda/glassmaps/MapDisplay.java
@@ -10,7 +10,9 @@ package com.example.cosda.glassmaps;
         import android.graphics.Path;
         import android.util.AttributeSet;
         import android.util.Log;
+        import android.view.Display;
         import android.view.View;
+        import android.widget.ImageView;
 
 public class MapDisplay extends View {
 
@@ -26,8 +28,8 @@ public class MapDisplay extends View {
     public int mapY;
     GPSTracker location = new GPSTracker(getContext());
     //resolution of map in glass
-    public final int mapResX = 1947;
-    public final int mapResY = 911;
+    public final int mapResX = 1996;
+    public final int mapResY = 815;
 
     public MapDisplay(Context c, AttributeSet attrs) {
         super(c, attrs);
@@ -44,18 +46,21 @@ public class MapDisplay extends View {
     protected void createBitmap() {
         mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
         mCanvas = new Canvas(mBitmap);
+
     }
     // override onDraw
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint p=new Paint();
-        Bitmap b= BitmapFactory.decodeResource(getResources(), R.drawable.wholedlsu);
-        canvas.drawColor(Color.GREEN);
-        mapPosition(location.latitude,location.longitude);
+        Bitmap b= BitmapFactory.decodeResource(getResources(), R.drawable.wholedlsuv3);
+        canvas.drawColor(Color.DKGRAY);
+        //mapPosition(location.latitude,location.longitude);
+        //mapPosition(14.565025, 120.993217);
         Log.d("lat", "lat" +location.latitude);
-        Log.d("long","long" +location.longitude);
-        canvas.drawBitmap(b, mapX, mapY, p);
+        Log.d("long", "long" + location.longitude);
+        //canvas.drawBitmap(b, mapX,mapY, p);
+        canvas.drawBitmap(b, w/2-mapResX,h/2-mapResY, p);
         p.setColor(Color.RED);
         p.setStyle(Paint.Style.FILL);
         canvas.drawCircle(w/2, h/2, 3, p);
@@ -64,18 +69,18 @@ public class MapDisplay extends View {
    public void mapPosition(double latitude, double longitude){
         //function to move the map position around, need to work on equation to get what i want.
        // latitude is y axis on world, longitude is x axis on world
-       final double latitudemin = 14.563425;
-       final double latitudemax = 14.567229;
-       final double longitudemin = 120.991574;
-       final double longitudemax = 120.994401;
+       final double latitudemin = 14.563446;
+       final double latitudemax = 14.567240;
+       final double longitudemin = 120.991874;
+       final double longitudemax = 120.994489;
        double latitudebetween = latitudemax-latitudemin;
        double longitudebetween = longitudemax-longitudemin;
        double latdelta = latitude - latitudemin;
        double longdelta = longitude - longitudemin;
        double latperc = latdelta  / latitudebetween;
        double longperc = longdelta / longitudebetween;
-        mapX=(int)(280-(mapResX*latperc));
-        mapY=(int)(120-(mapResY*longperc));
+       mapX=(int)(280-(mapResX*.58));
+       mapY=(int)(120-(mapResY*.41));
     }
 
 
